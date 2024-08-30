4100dae07253c44c5f8d5e30d7574b06028bb7eb
pietrofrizzi1 <147531549+pietrofrizzi1@users.noreply.github.com>
2024-08-30 14:09:30 +0200

    Add files via upload

---
+++ cmd/webapi/cors.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/handlers"
+)
+
+// applyCORSHandler applies a CORS policy to the router. CORS stands for Cross-Origin Resource Sharing: it's a security
+// feature present in web browsers that blocks JavaScript requests going across different domains if not specified in a
+// policy. This function sends the policy of this API server.
+func applyCORSHandler(h http.Handler) http.Handler {
+	return handlers.CORS(
+		handlers.AllowedHeaders([]string{
+			"content-type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "X-Requested-With", "Authorization",
+		}),
+		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"}),
+		handlers.AllowedOrigins([]string{"*"}),
+	)(h)
+}
